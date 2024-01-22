ba205e795361a55af10fe3272b524902774b9b71
Wakayo <58165469+Wakayo-Quozorox@users.noreply.github.com>
2024-01-22 16:59:50 +0100

    Merge generator.py in form.py

--- form.py
+++ form.py
@@ -12,7 +12,6 @@ import re
 
 import requests
 
-import generator
 
 # constants
 ALL_DATA_FIELDS = "FB_PUBLIC_LOAD_DATA_"
@@ -119,6 +118,25 @@ def get_form_submit_request(url: str, output = "console", only_required = False,
             print(f"Saved to {output}", flush = True)
     pass
 
+def generate_form_request_dict(entries, with_comment: bool = True):
+    """ Generate a dict of form request data from entries"""
+    result = "{\n"
+    for entry in entries:
+        if with_comment:
+            # gen name of entry
+            result += f"    # {entry['container_name']}{': ' + entry['name'] if entry['name'] else ''} {'(required)' * entry['required']}\n"
+            # gen all options (if any)
+            if entry['options']:
+                result += f"    #   Options: {entry['options']}\n"
+            else:
+                result += f"    #   Option: any text\n"
+        # gen entry id
+        result += f'    "entry.{entry["id"]}": "",\n'
+
+    result += "}"
+    return result
+
+        
 
 
 if __name__ == "__main__":
